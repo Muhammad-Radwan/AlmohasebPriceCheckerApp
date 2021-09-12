6c455dc9a36761ce08f18054049ae48ef146e1ba
Muhammad-Radwan <62462574+Muhammad-Radwan@users.noreply.github.com>
2021-09-12 23:56:21 +0200

    تم تعديل مجلد قراءة الأسعار وحذف الكود الغير مرغوب به

--- AlmohasebPriceCheckerAPI/Controllers/PriceCheckerController.cs
+++ AlmohasebPriceCheckerAPI/Controllers/PriceCheckerController.cs
@@ -11,17 +11,6 @@ namespace AlmohasebPriceCheckerAPI.Controllers
     public class PriceCheckerController : ApiController
     {
 
-    //    (from p in DBContext.TBL007
-    //                          join cu in DBContext.TBL128 on p.CardGuide equals cu.MainGuide into ntb
-    //                          from sub in ntb.DefaultIfEmpty()
-    //                          where p.Barcode.Contains(Barcode) || sub.Barcode.Contains(Barcode)
-    //                          select new
-    //                          {
-    //                              p.ProductName,
-    //                              p.Barcode,
-    //                              p.EndUserPrice,
-    //                              p.Unit
-    //});
         public IHttpActionResult GetPriceByBarcode(string Barcode)
         {
             using (AlmohasebSQLEntities cc = new AlmohasebSQLEntities())
